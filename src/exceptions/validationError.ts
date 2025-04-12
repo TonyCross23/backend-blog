101befcfd21b3950dcd3a1c82dcb2f2e8e5fd6c1
TonyCross23 <tonychamp40@gmail.com>
2025-04-12 21:49:15 +0630

    initial

--- src/exceptions/validationError.ts
+++
@@ -1,7 +0,0 @@
-import { HttpException } from "./root";
-
-export class UnprocessableEntity extends HttpException {
-  constructor(message: string, errorCode: any, error: any) {
-    super(message, errorCode, 422, error);
-  }
-}
--- src/servers/index.ts
+++
@@ -1,3 +0,0 @@
-import logger from "../config/loggers";
-import app from "./app";
-import { HOST, PORT } from "./configs";
