0d1896f753ecb434b8365b9d185a8f917d4afc66
TonyCross23 <tonychamp40@gmail.com>
2025-04-26 23:43:57 +0630

    doc-auth

--- src/route.ts
+++ src/route.ts
@@ -70,6 +70,51 @@ const route = (app: Express) => {
     createUserHandler
   );
 
+  /**
+   * @openapi
+   * '/api/login':
+   *  get:
+   *    tags:
+   *    - Session
+   *    summary: Get all sessions
+   *    responses:
+   *      200:
+   *        description: Get all sessions for current user
+   *        content:
+   *          application/json:
+   *            schema:
+   *              $ref: '#/components/schemas/GetSessionResponse'
+   *      403:
+   *        description: Forbidden
+   *  post:
+   *    tags:
+   *    - Session
+   *    summary: Create a session
+   *    requestBody:
+   *      required: true
+   *      content:
+   *        application/json:
+   *          schema:
+   *            $ref: '#/components/schemas/CreateSessionInput'
+   *    responses:
+   *      200:
+   *        description: Session created
+   *        content:
+   *          application/json:
+   *            schema:
+   *              $ref: '#/components/schemas/CreateSessionResponse'
+   *      401:
+   *        description: Unauthorized
+   *  delete:
+   *    tags:
+   *    - Session
+   *    summary: Delete a session
+   *    responses:
+   *      200:
+   *        description: Session deleted
+   *      403:
+   *        description: Forbidden
+   */
   app.post(
     "/api/login",
     validateRequest(createSessionSchema),
