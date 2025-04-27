7c8194f5c1d2fef29fcf7a62671a4be4a36dea83
TonyCross23 <tonychamp40@gmail.com>
2025-04-27 15:50:07 +0630

    deploy

--- config/default.ts
+++ config/default.ts
@@ -1,7 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export default {
   port: 1337,
   host: "localhost",
-  dbUri: "mongodb://localhost:27017/blog-app",
+  dbUri: process.env.DB_URI,
   saltWorkFactor: 10,
   accessTokenTtl: "3d",
   refreshTokenTtl: "1y",
